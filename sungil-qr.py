83d91dd1b3df09b73386a0b7baf7a17abfae949a
Lee Junghwan <hwan@sungil-i.kr>
2023-01-03 08:58:50 +0900

    Add files via upload

--- sungil-qr.py
+++ sungil-qr.py
@@ -30,31 +30,26 @@ import cv2
 import streamlit as st
 #import pyzbar.pyzbar as pyzbar
 
-try:
-    cap = cv2.VideoCapture(1)
+import cv2
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-    while cap.isOpened():
-        result, frame = cap.read()
-        if result:
-            #cv2.rectangle(img=frame, pt1=(100, 100), pt2=(300, 300), color=(255, 255, 255), thickness=3)
-            cv2.imshow("window", frame)
-            # barcodes = pyzbar.decode(frame)
-            # for barcode in barcodes:
-            #     #print(frame)
-            #     x, y, w, h = barcode.rect
-            #     frame = cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 3)
-            #     cv2.putText(frame, barcode.data.decode('utf-8'), (x, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (255, 0, 255), 1)
-            #
-            #     cv2.imshow("window", frame)
-                #cv2.rectangle(img = frame, pt1 = (100,100), pt2 = (300,300), color = (255,255,255), thickness = 3)
-                #
-                #print(barcode)
+faceCascade = cv2.CascadeClassifier(cv2.haarcascades+'haarcascade_frontalface_default.xml')
 
-            # if cv2.waitKey(1) == 27:
-            #     break
 
-        else:
-            print('error')
-            break
-except Exception as e:
-    print(e, 'errorTT')
+class VideoTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.i = 0
+
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+        i =self.i+1
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (95, 207, 30), 3)
+            cv2.rectangle(img, (x, y - 40), (x + w, y), (95, 207, 30), -1)
+            cv2.putText(img, 'F-' + str(i), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+
+        return img
+
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
