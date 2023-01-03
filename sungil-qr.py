55c996a360a326dcb01c30ea1debc1e2fc4e8238
Lee Junghwan <hwan@sungil-i.kr>
2023-01-03 16:30:16 +0900

    Add files via upload

--- sungil-qr.py
+++ sungil-qr.py
@@ -1,30 +1,48 @@
 import cv2
-import numpy as np
-import streamlit as st
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-from camera_input_live import camera_input_live
 
-"# Streamlit camera input live Demo"
-"## Try holding a qr code in front of your webcam"
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
 
-image = camera_input_live(debounce=100)
+        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
 
-if image is not None:
-    st.image(image)
-    bytes_data = image.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        return img
 
-    detector = cv2.QRCodeDetector()
 
-    data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
 
-    if data:
-        st.write("# Found QR code")
-        st.write(data)
-        with st.expander("Show details"):
-            st.write("BBox:", bbox)
-            st.write("Straight QR code:", straight_qrcode)
 
+
+
+# import cv2
+# import numpy as np
+# import streamlit as st
+#
+# from camera_input_live import camera_input_live
+#
+# "# Streamlit camera input live Demo"
+# "## Try holding a qr code in front of your webcam"
+#
+# image = camera_input_live(debounce=1000)
+#
+# if image is not None:
+#     st.image(image)
+#     bytes_data = image.getvalue()
+#     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+#
+#     detector = cv2.QRCodeDetector()
+#
+#     data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
+#
+#     if data:
+#         st.write("# Found QR code")
+#         st.write(data)
+#         with st.expander("Show details"):
+#             st.write("BBox:", bbox)
+#             st.write("Straight QR code:", straight_qrcode)
+#
 
 #import pyzbar.pyzbar as pyzbar
 # import streamlit as st
