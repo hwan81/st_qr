d63c3f9c77b6e03bfe25c75552c33992928b6ebf
Lee Junghwan <hwan@sungil-i.kr>
2023-01-03 09:12:23 +0900

    Add files via upload

--- sungil-qr.py
+++ sungil-qr.py
@@ -30,5 +30,22 @@ import cv2
 import streamlit as st
 #import pyzbar.pyzbar as pyzbar
 import streamlit as st
+import cv2
 
-picture = st.camera_input("Take a picture")
+image = st.camera_input("Take a picture")
+
+if image is not None:
+    st.image(image)
+    bytes_data = image.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+    detector = cv2.QRCodeDetector()
+
+    data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
+
+    if data:
+        st.write("# Found QR code")
+        st.write(data)
+        with st.expander("Show details"):
+            st.write("BBox:", bbox)
+            st.write("Straight QR code:", straight_qrcode)
