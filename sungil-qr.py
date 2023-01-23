e12d54af3c7aba04a34435f9d123fcb83709f71a
Lee Junghwan <hwan@sungil-i.kr>
2023-01-23 11:16:40 +0900

    Add files via upload

--- sungil-qr.py
+++ sungil-qr.py
@@ -1,69 +1,20 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
-import cv2
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
+from streamlit_qrcode_scanner import qrcode_scanner
+from camera_input_live import camera_input_live
 
-    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+step1, step2, step3 = st.tabs(["STEP1", "STEP2", "STEP3"])
 
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-
-webrtc_streamer(key="example", video_frame_callback=callback)
+with step1:
+    st.write("s1")
+    spread_id = st.text_input('insert google spreadsheet id')
 
 
+with step2:
+    # c = camera_input_live()
+    # if c:
+    #     st.image(c)
+    qr_code = qrcode_scanner(key='qrcode_scanner')
 
-
-# import cv2
-# import numpy as np
-# import streamlit as st
-#
-# from camera_input_live import camera_input_live
-#
-# "# Streamlit camera input live Demo"
-# "## Try holding a qr code in front of your webcam"
-#
-# image = camera_input_live(debounce=1000)
-#
-# if image is not None:
-#     st.image(image)
-#     bytes_data = image.getvalue()
-#     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-#
-#     detector = cv2.QRCodeDetector()
-#
-#     data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
-#
-#     if data:
-#         st.write("# Found QR code")
-#         st.write(data)
-#         with st.expander("Show details"):
-#             st.write("BBox:", bbox)
-#             st.write("Straight QR code:", straight_qrcode)
-#
-
-#import pyzbar.pyzbar as pyzbar
-# import streamlit as st
-# import cv2
-# import numpy as np
-#
-# image = st.camera_input("Take a picture")
-# st.write('image start')
-# if image is not None:
-#     image = st.camera_input("Take a picture")
-#     st.write("image capturing")
-#     #st.image(image)
-#     bytes_data = image.getvalue()
-#     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-#
-#     detector = cv2.QRCodeDetector()
-#     st.write(detector)
-#     data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
-#
-#     if data:
-#         st.write("# Found QR code")
-#         st.write(data)
-#         with st.expander("Show details"):
-#             st.write("BBox:", bbox)
-#             st.write("Straight QR code:", straight_qrcode)
+    if qr_code:
+        st.write(qr_code)
+    st.write('s2')
